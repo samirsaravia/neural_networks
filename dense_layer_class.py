2f8e5b1fccf1039af6cab51180d6b45bcfec94be
Samir <51790766+samirsaravia@users.noreply.github.com>
2022-03-21 14:20:00 -0300

    comment out

--- dense_layer_class.py
+++ dense_layer_class.py
@@ -1,6 +1,6 @@
 import numpy as np
-from nnfs.datasets import spiral_data
-import matplotlib.pyplot as plt
+# from nnfs.datasets import spiral_data
+# import matplotlib.pyplot as plt
 
 
 class Layer_Dense:
@@ -16,13 +16,13 @@ class Layer_Dense:
 
 
 # create a dataset
-x, y = spiral_data(samples=100, classes=3)
-
-# Create dense layer with 2 input features and 3 output values
-dense1 = Layer_Dense(2, 3)
-
-# Perform a forward pass of our training data through this layer
-dense1.forward(x)
-
-#  Let's see output of the first few samples
-print(dense1.output[:])
+# x, y = spiral_data(samples=100, classes=3)
+#
+# # Create dense layer with 2 input features and 3 output values
+# dense1 = Layer_Dense(2, 3)
+#
+# # Perform a forward pass of our training data through this layer
+# dense1.forward(x)
+#
+# #  Let's see output of the first few samples
+# # print(dense1.output[:])
